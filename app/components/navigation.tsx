cce5eab9d5df77e4d855a4a046eebd23e0eeda0e
sia <parkmy722698@gmail.com>
2025-07-04 18:02:44 +0900

    Feat. main 작업 중

---
+++ app/components/navigation.tsx
@@ -0,0 +1,30 @@
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "~/components/ui/navigation-menu";
+
+export default function Navigation() {
+  return (
+    <div className='flex justify-center align-center h-10'>
+      Book Feel
+      <div>
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <NavigationMenuLink>Link</NavigationMenuLink>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
+    </div>
+  );
+}
