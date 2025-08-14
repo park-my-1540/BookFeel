c2c7fb52855950e4a66aaca6b545ce5f3fb59c8b
sia <parkmy722698@gmail.com>
2025-08-14 19:17:45 +0900

    Feat. shopping_Cart table pk : itemId + profile_id 복합키로 변경

--- app/features/shoppingcart/schema.ts
+++ app/features/shoppingcart/schema.ts
@@ -1,17 +1,32 @@
-import { pgTable, uuid, text, timestamp, bigint } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  uuid,
+  text,
+  timestamp,
+  bigint,
+  primaryKey,
+} from "drizzle-orm/pg-core";
 import { profiles } from "../users/schema";
 
-export const shopping_cart = pgTable("shopping_cart", {
-  profile_id: uuid().references(() => profiles.profile_id, {
-    onDelete: "cascade",
-  }),
-  itemId: text().notNull(),
-  title: text().notNull(),
-  author: text().notNull(),
-  thumbnail: text().notNull(),
-  price: bigint({ mode: "number" }),
-  sale_price: bigint({ mode: "number" }),
-  contents: text(),
-  created_at: timestamp().notNull().defaultNow(),
-  updated_at: timestamp().notNull().defaultNow(),
-});
+export const shopping_cart = pgTable(
+  "shopping_cart",
+  {
+    profile_id: uuid()
+      .references(() => profiles.profile_id, {
+        onDelete: "cascade",
+      })
+      .notNull(),
+    itemId: text().notNull(),
+    title: text().notNull(),
+    author: text().notNull(),
+    thumbnail: text().notNull(),
+    price: bigint({ mode: "number" }),
+    sale_price: bigint({ mode: "number" }),
+    contents: text(),
+    created_at: timestamp().notNull().defaultNow(),
+    updated_at: timestamp().notNull().defaultNow(),
+  },
+  (table) => ({
+    pk: primaryKey({ columns: [table.profile_id, table.itemId] }),
+  })
+);
