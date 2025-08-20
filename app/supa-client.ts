6f0db4552877fc47db2c279f146b3f0ec7cb79de
sia <parkmy722698@gmail.com>
2025-08-20 23:48:31 +0900

    fix.

--- app/supa-client.ts
+++ app/supa-client.ts
@@ -7,18 +7,15 @@ import {
 import { createClient } from "@supabase/supabase-js";
 
 export const browserClient = createBrowserClient<any>(
-  process.env.SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!
+  "https://spmbavubrdyesqqcfeim.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNwbWJhdnVicmR5ZXNxcWNmZWltIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1Mzg3Mzg4MCwiZXhwIjoyMDY5NDQ5ODgwfQ.c5_I9-WqlhZpz22PpPg1kX96OWYPhaad21Da8-5-2Uo"
 );
-
-export const supabaseUrl = process.env.SUPABASE_URL!;
-export const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!; // 서비스 롤 키
 
 export const makeSSRClient = (request: Request) => {
   const headers = new Headers();
   const serverSideClient = createServerClient<any, "public">(
-    supabaseUrl,
-    supabaseServiceKey,
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_ANON_KEY!,
     {
       cookies: {
         getAll() {
@@ -49,8 +46,12 @@ export const makeSSRClient = (request: Request) => {
   };
 };
 
-export const adminClient = createClient(supabaseUrl, supabaseServiceKey, {
-  auth: {
-    persistSession: false,
-  },
-});
+export const adminClient = createClient(
+  process.env.SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY!,
+  {
+    auth: {
+      persistSession: false,
+    },
+  }
+);
