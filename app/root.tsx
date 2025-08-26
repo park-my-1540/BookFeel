d0ec708bd2d1c0e3605ceeb0a9db832c631579b2
sia <parkmy722698@gmail.com>
2025-08-26 16:03:15 +0900

    Feat. Add meta icon

--- app/root.tsx
+++ app/root.tsx
@@ -14,15 +14,10 @@ import { getUserProfileById } from "./features/users/queries";
 import { makeSSRClient } from "./supa-client";
 
 export const links: Route.LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
   {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+    rel: "icon",
+    href: "/icon.png", // public 폴더에 favicon.ico 두기
+    type: "image/x-icon",
   },
 ];
 
